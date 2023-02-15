ba2cfde466186f90d339fab989fd8b5360bac34c
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-15 21:15:06 +0200

    add new feature drawing-screen.dart add new route in routes

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/features/drawing-screen/presentation/screen/drawing-screen.dart';
 
 import '../../core/utils/app_strings.dart';
 import '../../features/home/presentation/screens/home_screen.dart';
@@ -7,20 +8,34 @@ import '../../features/splash/presentation/screens/splash_screen.dart';
 class Routes {
   static const String initial = '/';
   static const String appHome = '/app-home';
+  static const String drawingScreen = '/app-drawing-screen';
 }
 
 class AppRoutes {
   static Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case Routes.initial:
-        return MaterialPageRoute(builder: (context,) {
-          return const SplashScreen();
-        }, settings: routeSettings);
+        return MaterialPageRoute(
+            builder: (
+              context,
+            ) {
+              return const SplashScreen();
+            },
+            settings: routeSettings);
 
       case Routes.appHome:
-        return MaterialPageRoute(builder: (context) {
-          return const HomeScreen();
-        }, settings: routeSettings);
+        return MaterialPageRoute(
+            builder: (context) {
+              return const HomeScreen();
+            },
+            settings: routeSettings);
+
+      case Routes.drawingScreen:
+        return MaterialPageRoute(
+            builder: (context) {
+              return const DrawingScreen();
+            },
+            settings: routeSettings);
 
       default:
         return undefinedRoute();
@@ -29,11 +44,10 @@ class AppRoutes {
 
   static Route<dynamic> undefinedRoute() {
     return MaterialPageRoute(
-        builder: ((context) =>
-        const Scaffold(
-          body: Center(
-            child: Text(AppStrings.noRouteFound),
-          ),
-        )));
+        builder: ((context) => const Scaffold(
+              body: Center(
+                child: Text(AppStrings.noRouteFound),
+              ),
+            )));
   }
 }
---
+++ lib/features/drawing-screen/presentation/screen/drawing-screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DrawingScreen> createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
