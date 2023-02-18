535deca7bc5376fb910f68a523aaf413e68f21ea
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-18 13:11:35 +0200

    add packages add drawing feature

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -13,7 +13,7 @@ class AppColors {
   static Color fontPrimary = HexColor('#FFFFFF');
   static Color fontSecondary = HexColor('#E8C39E');
 
-  static Color border = HexColor('#2F2C79');
+  static Color border = HexColor('#B3B4B4');
 
   static Color button = HexColor('#07ABB3');
   static Color snackbar = HexColor('#2F2C79');
--- lib/features/drawing-screen/presentation/screen/drawing-screen.dart
+++ lib/features/drawing-screen/presentation/screen/drawing-screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_starter/core/utils/app_colors.dart';
 import 'package:flutter_starter/core/widgets/buttons/button_widget.dart';
 import 'package:flutter_starter/features/drawing-screen/presentation/widget/app-bar.dart';
+import 'package:flutter_starter/features/drawing-screen/presentation/widget/drawing-part.dart';
 
 class DrawingScreen extends StatefulWidget {
   const DrawingScreen({Key? key}) : super(key: key);
@@ -16,11 +17,18 @@ class _DrawingScreenState extends State<DrawingScreen> {
     return Material(
       child: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              AppBarWidget(),
-              ButtonWidget(text: "Next", Icon: Icons.arrow_forward),
-            ],
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                AppBarWidget(),
+                Drawing(),
+                SizedBox(
+                  height: 10,
+                ),
+                ButtonWidget(text: "Next", Icon: Icons.arrow_forward),
+              ],
+            ),
           ),
         ),
       ),
