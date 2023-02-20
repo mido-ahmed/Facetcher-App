3cf2dfa5fad36e5e9cbc743b2a4357aea20d89d5
Henry-Azer <henryazer@outlook.com>
2023-02-20 19:51:46 +0200

    refactor home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/core/widgets/navigator/navigation_bar_wrapper.dart';
 
-import '../../../../config/routes/app_routes.dart';
+import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_text_style.dart';
-import '../../../../core/utils/assets_manager.dart';
-import '../../../../core/widgets/navigation_bar_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,43 +12,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
+  bool _toggleNavigationBar = false;
+
+  void _handleToggleNavigationBar() {
+    setState(() {
+      _toggleNavigationBar = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-          appBar: AppBar(
-            centerTitle: false,
-            title: Padding(
-              padding: const EdgeInsets.only(top: 30.0, left: 30.0),
-              child: Text("Hello Developer!", style: AppTextStyle.homeText,),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(top: 25.0, right: 35.0),
-                child: GestureDetector(
-                  onTap: () {
-                    // TODO : change route to whatever you want
-                    Navigator.pushReplacementNamed(context, Routes.appHome);
-                  },
-                  child: Image.asset(
-                    ImgAssets.user,
-                    width: 50,
-                    height: 50,
-                  ),
+    return NavigationBarWrapper(
+      toggleNavigationBar: _toggleNavigationBar,
+      path: ModalRoute.of(context)?.settings.name,
+      child: Material(
+        child: Scaffold(
+            appBar: AppBar(
+              centerTitle: false,
+              title: Padding(
+                padding: const EdgeInsets.only(top: 30.0, left: 30.0),
+                child: Text(
+                  "Hello Developer!",
+                  style: AppTextStyle.homeText,
                 ),
               ),
-            ],
-          ),
-          extendBody: true,
-          resizeToAvoidBottomInset: true,
-          bottomNavigationBar: NavigationBarWidget(path: ModalRoute.of(context)?.settings.name),
-          body: Center(child: Text("Start Coding!", style: AppTextStyle.homeText,),
-          )),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 25.0, right: 35.0),
+                  child: IconButton(
+                      onPressed: () => _handleToggleNavigationBar(),
+                      icon: const Icon(
+                        Icons.menu,
+                        size: 30,
+                      ),
+                      color: AppColors.white),
+                ),
+              ],
+            ),
+            extendBody: true,
+            resizeToAvoidBottomInset: true,
+            body: Center(
+              child: Text(
+                "Home",
+                style: AppTextStyle.homeText,
+              ),
+            )),
+      ),
     );
   }
 }
