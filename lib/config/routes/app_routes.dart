aa1a2b2a386bdf70862eb3cc3fabf7e71d4676b2
Henry-Azer <henryazer@outlook.com>
2023-02-20 19:52:54 +0200

    add to app routes, user profile and history screens

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/features/user-history/presentation/screens/user_history_screen.dart';
 
 import '../../core/utils/app_strings.dart';
 import '../../features/home/presentation/screens/home_screen.dart';
 import '../../features/splash/presentation/screens/splash_screen.dart';
+import '../../features/user-profile/presentation/screens/user_profile_screen.dart';
 
 class Routes {
   static const String initial = '/';
   static const String appHome = '/app-home';
+  static const String userHistory = '/user-history';
+  static const String userProfile = '/user-profile';
 }
 
 class AppRoutes {
@@ -20,6 +24,16 @@ class AppRoutes {
       case Routes.appHome:
         return MaterialPageRoute(builder: (context) {
           return const HomeScreen();
+        }, settings: routeSettings);
+
+      case Routes.userHistory:
+        return MaterialPageRoute(builder: (context) {
+          return const UserHistoryScreen();
+        }, settings: routeSettings);
+
+      case Routes.userProfile:
+        return MaterialPageRoute(builder: (context) {
+          return const UserProfileScreen();
         }, settings: routeSettings);
 
       default:
