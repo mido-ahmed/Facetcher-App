14cdccb17cc316abfd6f813d8554cb09466069b5
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-24 18:12:08 +0200

    create custom widget for list title

---
+++ lib/core/widgets/list_title_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_text_style.dart';
+
+class ListTitleWidget extends StatefulWidget {
+  final String title;
+  final String subtitle;
+
+  const ListTitleWidget({Key? key, required this.title, required this.subtitle})
+      : super(key: key);
+
+  @override
+  _ListTitleWidgetState createState() => _ListTitleWidgetState();
+}
+
+class _ListTitleWidgetState extends State<ListTitleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        widget.title,
+        style: AppTextStyle.drawingScreenTitle,
+      ),
+      subtitle: Text(
+        widget.subtitle,
+        maxLines: 3,
+        style: AppTextStyle.drawingScreenTitleDetails,
+      ),
+    );
+  }
+}
