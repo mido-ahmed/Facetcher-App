3a7d49287fc02954afbaac60453474e2dbf29be7
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-24 18:11:45 +0200

    edit in screen

--- lib/features/drawing-report/presentation/widgets/face_details.dart
+++ lib/features/drawing-report/presentation/widgets/face_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/core/utils/app_text_style.dart';
+import 'package:flutter_starter/core/widgets/list_title_widget.dart';
 
 class FaceDetails extends StatefulWidget {
   const FaceDetails({Key? key}) : super(key: key);
@@ -11,44 +12,67 @@ class FaceDetails extends StatefulWidget {
 class _FaceDetailsState extends State<FaceDetails> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Text(
-              "Title :",
-              style: AppTextStyle.editButtonText,
-            ),
-            Text(
-              "title",
-              style: AppTextStyle.textDescribtion,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              "Gender :",
-              style: AppTextStyle.editButtonText,
-            ),
-            Text(
-              "Male",
-              style: AppTextStyle.textDescribtion,
-            ),
-          ],
-        ),
-        ListTile(
-          title:Text(
-            "Description :",
-            style: AppTextStyle.editButtonText,
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 20,
           ),
-          subtitle:   Text(
-            "Description: Lorem ipsum dolor sit amet consectetur. Eleifend fringilla odio iaculis tortor faucibus netus nisl pulvinar mollis. Mattis nisl aliquam nunc aliquam proin odio. Consequat amet tortor facilisis quam sit. Adipiscing interdum pellentesque elementum ut... ",
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Details",
+                style: AppTextStyle.textDrawingReport,
+              ),
+              Image.asset('assets/icons/Vector.png'),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Text(
+                "Title :",
+                style: AppTextStyle.editButtonText,
+              ),
+              Text(
+                "title",
+                style: AppTextStyle.textDescribtion,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Text(
+                "Gender :",
+                style: AppTextStyle.editButtonText,
+              ),
+              Text(
+                "Male",
+                style: AppTextStyle.textDescribtion,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Description :Lorem ipsum dolor  sit amet consectetur. Eleifend fringillaodio iaculis tortor faucibus netus nisl pulvinar mollis. Mattis nisl aliquam  nunc aliquam proin odio. Consequat amet tortor facilisis quam sit. Adipiscing interdum pellentesque elementum ut... ",
+            //maxLines: 5,
             style: AppTextStyle.textDescribtion,
           ),
-        ),
-      ],
+          SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
     );
   }
 }
