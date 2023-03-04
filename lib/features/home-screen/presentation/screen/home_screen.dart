c98f43b1f132edd7f78f4cdbadb99303ba1168d3
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-03-04 11:38:10 +0200

    add new feature have menu icon that do some functions circle avatar take me to user profile

--- lib/features/home-screen/presentation/screen/home_screen.dart
+++ lib/features/home-screen/presentation/screen/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/core/widgets/navigator/navigation_bar_wrapper.dart';
+import 'package:flutter_starter/features/home-screen/presentation/widget/circle_profile.dart';
+
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/widgets/app_bar_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,8 +13,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _toggleNavigationBar = false;
+
+  void _handleToggleNavigationBar() {
+    setState(() {
+      _toggleNavigationBar = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return NavigationBarWrapper(
+      toggleNavigationBar: _toggleNavigationBar,
+      path: ModalRoute.of(context)?.settings.name,
+      child: Material(
+        child: Scaffold(
+          extendBody: true,
+          resizeToAvoidBottomInset: true,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  AppBarWidget(
+                    leftChild: CircularProfile(),
+                    rightChild: IconButton(
+                        onPressed: () => _handleToggleNavigationBar(),
+                        icon: const Icon(
+                          Icons.menu,
+                          size: 30,
+                        ),
+                        color: AppColors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
