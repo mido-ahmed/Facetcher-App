b7676b16f89bd4858abf92273ecb805217736a6e
Tota <71022738+Tota6@users.noreply.github.com>
2023-03-16 00:59:25 +0200

    adding the animation needed in the splash_screen

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
 import '../../../../config/routes/app_routes.dart';
-import '../../../../core/utils/app_text_style.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,7 +9,8 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
   late Timer _timer;
 
   @override
@@ -27,16 +25,27 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
     super.dispose();
   }
 
-  _startDelay() {_timer = Timer(const Duration(milliseconds: 8000), () => _goNext());}
+  _startDelay() {
+    _timer = Timer(const Duration(milliseconds: 8000), () => _goNext());
+  }
 
-  _goNext() => {Navigator.pushReplacementNamed(context, Routes.appHome)};
+  _goNext() => {Navigator.pushReplacementNamed(context, Routes.login)};
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
         body: Center(
-          child: Text("Splash Screen", style: AppTextStyle.homeText,),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedSize(
+                duration: const Duration(seconds: 2),
+                curve: Curves.elasticInOut,
+                child: Image.asset('assets/images/2x/Group.png'),
+              ),
+            ],
+          ),
         ),
       ),
     );
