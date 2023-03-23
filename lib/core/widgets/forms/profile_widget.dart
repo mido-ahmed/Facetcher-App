2f4f457429be0cd9d7a585842dbb77683fefdadb
Tota <71022738+Tota6@users.noreply.github.com>
2023-03-23 22:37:16 +0200

    editing the user nice name by user email and changing user image by user url to pass from the network

---
+++ lib/core/widgets/forms/profile_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/app_colors.dart';
+import '../../utils/app_text_style.dart';
+
+class ProfileWidget extends StatefulWidget {
+  final String? userProfileName;
+  final String userEmail;
+  final String userProfileUrl;
+
+  const ProfileWidget({
+    Key? key,
+    required this.userProfileName,
+    required this.userEmail,
+    required this.userProfileUrl,
+  }) : super(key: key);
+
+  @override
+  State<ProfileWidget> createState() => _ProfileWidgetState();
+}
+
+class _ProfileWidgetState extends State<ProfileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: AppColors.button,
+          radius: 50,
+          child: Image.network(
+            widget.userProfileUrl,
+            scale: 0.6,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: widget.userProfileName,
+                    style: AppTextStyle.userProfileTitle,
+                  ),
+                  TextSpan(
+                    text: '\n${widget.userEmail}',
+                    style: AppTextStyle.userProfileDetails,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
