c8f7d365d1261803382eaa92208d93244498e132
Tota <71022738+Tota6@users.noreply.github.com>
2023-03-26 01:00:44 +0200

    change the image to be widget to take both image network and image assets

--- lib/core/widgets/forms/profile_widget.dart
+++ lib/core/widgets/forms/profile_widget.dart
@@ -1,4 +1,64 @@
+// import 'package:flutter/material.dart';
+//
+// import '../../utils/app_colors.dart';
+// import '../../utils/app_text_style.dart';
+//
+// class ProfileWidget extends StatefulWidget {
+//   final String? userProfileName;
+//   final String userEmail;
+//   final String userProfileUrl;
+//
+//   const ProfileWidget({
+//     Key? key,
+//     required this.userProfileName,
+//     required this.userEmail,
+//     required this.userProfileUrl,
+//   }) : super(key: key);
+//
+//   @override
+//   State<ProfileWidget> createState() => _ProfileWidgetState();
+// }
+//
+// class _ProfileWidgetState extends State<ProfileWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//       children: [
+//         CircleAvatar(
+//           backgroundColor: AppColors.button,
+//           radius: 50,
+//           child: Image.asset(widget.userProfileUrl),
+//         ),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             RichText(
+//               textAlign: TextAlign.center,
+//               text: TextSpan(
+//                 children: [
+//                   TextSpan(
+//                     text: widget.userProfileName,
+//                     style: AppTextStyle.userProfileTitle,
+//                   ),
+//                   TextSpan(
+//                     text: '\n${widget.userEmail}',
+//                     style: AppTextStyle.userProfileDetails,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../utils/app_colors.dart';
 import '../../utils/app_text_style.dart';
@@ -6,7 +66,7 @@ import '../../utils/app_text_style.dart';
 class ProfileWidget extends StatefulWidget {
   final String? userProfileName;
   final String userEmail;
-  final String userProfileUrl;
+  final Widget userProfileUrl;
 
   const ProfileWidget({
     Key? key,
@@ -28,11 +88,8 @@ class _ProfileWidgetState extends State<ProfileWidget> {
         CircleAvatar(
           backgroundColor: AppColors.button,
           radius: 50,
-          child: Image.network(
-            widget.userProfileUrl,
-            scale: 0.6,
+          child: widget.userProfileUrl
           ),
-        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
