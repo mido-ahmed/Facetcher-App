ce6f2e72d56e4e8b8a51b6b6c003c47290048bb5
Henry-Azer <henryazer@outlook.com>
2023-04-01 21:56:04 +0200

    refactor user submission repository filename in usecase

--- lib/features/drawing-details/domain/usecases/create_user_submission_usecase.dart
+++ lib/features/drawing-details/domain/usecases/create_user_submission_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:facetcher/data/entities/user-submission/user_submission_request.dart';
 import 'package:facetcher/data/models/user-submission/user_submission.dart';
-import 'package:facetcher/data/repositories/user-submission/user_submission.dart';
+import 'package:facetcher/data/repositories/user-submission/user_submission_repository.dart';
 
 import '../../../../core/error/exceptions.dart';
 import '../../../../core/models/response_model.dart';
