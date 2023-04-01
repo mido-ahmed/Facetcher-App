4800d5c2a08d474848ec1e6447e305883e1798c1
Henry-Azer <henryazer@outlook.com>
2023-04-01 03:27:01 +0200

    add user icon and shape to image network assets

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -8,4 +8,6 @@ class ImgAssets {
 class ImageNetwork {
   static const String logo = 'https://d178ormcbshsy5.cloudfront.net/logo.png';
   static const String logoFlat = 'https://d178ormcbshsy5.cloudfront.net/logo_flat.png';
+  static const String userIcon = 'https://d178ormcbshsy5.cloudfront.net/user_icon.png';
+  static const String userShape = 'https://d178ormcbshsy5.cloudfront.net/user_shape.png';
 }
