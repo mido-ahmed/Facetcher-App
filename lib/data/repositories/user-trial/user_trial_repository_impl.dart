789c46d3043a25f2669272388ba1659cc28469de
Henry-Azer <henryazer@outlook.com>
2023-04-01 23:34:57 +0200

    implement user trial repository and it's impl with create user trial function

---
+++ lib/data/repositories/user-trial/user_trial_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:facetcher/data/repositories/user-trial/user_trial_repository.dart';
+
+import '../../../core/error/exceptions.dart';
+import '../../../core/models/response_model.dart';
+import '../../../core/network/network_info.dart';
+import '../../datasources/user-trial/user_trial_local_datasource.dart';
+import '../../datasources/user-trial/user_trial_remote_datasource.dart';
+import '../../entities/user-trial/user_trial_request.dart';
+import '../../models/user-trial/user_trial.dart';
+
+class UserTrialRepositoryImpl implements UserTrialRepository {
+  final NetworkInfo networkInfo;
+  final UserTrialLocalDataSource userTrialLocalDataSource;
+  final UserTrialRemoteDataSource userTrialRemoteDataSource;
+
+  UserTrialRepositoryImpl(
+      {required this.networkInfo,
+      required this.userTrialLocalDataSource,
+      required this.userTrialRemoteDataSource});
+
+  @override
+  Future<Either<GenericException, ResponseModel<UserTrial>>> createUserTrial(UserTrialRequest userTrialRequest) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final responseUserSubmission = await userTrialRemoteDataSource.createUserTrial(userTrialRequest);
+        return Right(responseUserSubmission);
+      } on GenericException catch (exception) {
+        return Left(exception);
+      }
+    } else {
+      return const Left(CacheException());
+    }
+  }
+}
