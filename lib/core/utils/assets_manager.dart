0774f6fae110276c2ea1245e848c0004130edfd1
Henry-Azer <henryazer@outlook.com>
2023-04-03 01:24:38 +0200

    add toolbar icons to image network assets

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -10,5 +10,13 @@ class ImageNetwork {
   static const String logoFlat = 'https://d178ormcbshsy5.cloudfront.net/logo_flat.png';
   static const String userIcon = 'https://d178ormcbshsy5.cloudfront.net/user_icon.png';
   static const String userShape = 'https://d178ormcbshsy5.cloudfront.net/user_shape.png';
+
+  static const String penIcon = 'https://d178ormcbshsy5.cloudfront.net/pen_draw_icon_2.png';
+  static const String penFilledIcon = 'https://d178ormcbshsy5.cloudfront.net/pen_draw_filled_icon.png';
   static const String editIcon = 'https://d178ormcbshsy5.cloudfront.net/edit_icon.png';
+  static const String undoIcon = 'https://d178ormcbshsy5.cloudfront.net/undo_draw_icon.png';
+  static const String redoIcon = 'https://d178ormcbshsy5.cloudfront.net/redo_draw_icon.png';
+  static const String binIcon = 'https://d178ormcbshsy5.cloudfront.net/eraser_draw_icon.png';
+  static const String eraserIcon = 'https://d178ormcbshsy5.cloudfront.net/eraser_2_draw_icon.png';
+  static const String eraserFilledIcon = 'https://d178ormcbshsy5.cloudfront.net/eraser_filled_draw_icon_2.png';
 }
