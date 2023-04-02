466149a990b6c3d81625beb43d3275df27801f7a
Henry-Azer <henryazer@outlook.com>
2023-04-02 04:00:44 +0200

    refactor user trial request image data type

--- lib/data/entities/user-trial/user_trial_request.dart
+++ lib/data/entities/user-trial/user_trial_request.dart
@@ -1,18 +1,17 @@
+import 'dart:typed_data';
 import 'package:equatable/equatable.dart';
 
 class UserTrialRequest extends Equatable {
-  final String image;
+  final Uint8List image;
   final int submissionId;
 
-  const UserTrialRequest(
-      {required this.image,
-      required this.submissionId});
+  const UserTrialRequest({required this.image, required this.submissionId});
 
   @override
   List<Object?> get props => [];
 
   Map<String, dynamic> toJson() => {
-        "inputImageFile": image,
-        "userSubmissionId": submissionId,
-      };
+    "inputImageFile": image,
+    "userSubmissionId": submissionId,
+  };
 }
