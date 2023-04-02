c5bc481a610a99537c7e318009736f06f5e54794
Henry-Azer <henryazer@outlook.com>
2023-04-02 16:22:41 +0200

    add to List Title widget text aligning

--- lib/core/widgets/list_title_widget.dart
+++ lib/core/widgets/list_title_widget.dart
@@ -4,9 +4,17 @@ import '../utils/app_text_style.dart';
 
 class ListTitleWidget extends StatefulWidget {
   final String title;
+  final TextAlign titleAlign;
   final String subtitle;
+  final TextAlign subtitleAlign;
 
-  const ListTitleWidget({Key? key, required this.title, required this.subtitle}) : super(key: key);
+  const ListTitleWidget(
+      {Key? key,
+      required this.title,
+      required this.subtitle,
+      required this.titleAlign,
+      required this.subtitleAlign})
+      : super(key: key);
 
   @override
   State<ListTitleWidget> createState() => _ListTitleWidgetState();
@@ -18,11 +26,13 @@ class _ListTitleWidgetState extends State<ListTitleWidget> {
     return ListTile(
       title: Text(
         widget.title,
+        textAlign: widget.titleAlign,
         style: AppTextStyle.drawingScreenTitle,
       ),
       subtitle: Text(
         widget.subtitle,
         maxLines: 3,
+        textAlign: widget.subtitleAlign,
         style: AppTextStyle.drawingScreenTitleDetails,
       ),
     );
