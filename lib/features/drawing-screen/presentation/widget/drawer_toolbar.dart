cbc70415915664f0ed1b5d34ef0f9b7d98876d80
Henry-Azer <henryazer@outlook.com>
2023-04-03 01:24:01 +0200

    refactor drawer toolbar design

--- lib/features/drawing-screen/presentation/widget/drawer_toolbar.dart
+++ lib/features/drawing-screen/presentation/widget/drawer_toolbar.dart
@@ -1,10 +1,10 @@
+import 'package:facetcher/core/utils/assets_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:scribble/scribble.dart';
 
 import '../../../../core/utils/app_colors.dart';
-import 'drawer_widget.dart';
 
 class DrawerToolbar extends StatelessWidget {
   final ScribbleNotifier notifier;
@@ -21,29 +21,29 @@ class DrawerToolbar extends StatelessWidget {
     return StateNotifierBuilder<ScribbleState>(
       stateNotifier: notifier,
       builder: (context, state, _) => Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildUndoButton(context),
-            const Divider(
-              height: 2.0,
-            ),
-            _buildRedoButton(context),
-            const Divider(
-              height: 2.0,
-            ),
-            _buildClearButton(context),
-            const Divider(
-              height: 4.0,
-            ),
-            _buildEraserButton(context, isSelected: state is Erasing),
-            _buildColorButton(context,
-                color: AppColors.black,
-                state: state,
-                icon: const Icon(Icons.add)),
-            _buildStrokeToolbar(context),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildUndoButton(context),
+                  _buildRedoButton(context),
+                  _buildClearButton(context),
+                  _buildColorButton(context, color: AppColors.black, isSelected: state is Drawing),
+                  _buildEraserButton(context, isSelected: state is Erasing),
+                  const SizedBox(width: 10,),
+                  _buildStrokeToolbar(context),
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: const Divider(height: 1.5, color: Colors.grey,),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -71,9 +71,9 @@ class DrawerToolbar extends StatelessWidget {
   }) {
     final selected = state.selectedWidth == strokeWidth;
     return Padding(
-      padding: const EdgeInsets.all(4),
+      padding: const EdgeInsets.symmetric(horizontal: 4),
       child: Material(
-        elevation: selected ? 4 : 0,
+        elevation: selected ? 5 : 0,
         shape: const CircleBorder(),
         child: InkWell(
           onTap: () => notifier.setStrokeWidth(strokeWidth),
@@ -85,11 +85,11 @@ class DrawerToolbar extends StatelessWidget {
             decoration: BoxDecoration(
                 color: state.map(
                   drawing: (s) => Color(s.selectedColor),
-                  erasing: (_) => Colors.transparent,
+                  erasing: (_) => AppColors.textSecondary,
                 ),
                 border: state.map(
-                  drawing: (_) => null,
-                  erasing: (_) => Border.all(width: 5),
+                  drawing: (_) => selected ? Border.all(width: 3, color: AppColors.textSecondary2) : null,
+                  erasing: (_) => selected ? Border.all(width: 3, color: AppColors.textSecondary2) : null,
                 ),
                 borderRadius: BorderRadius.circular(50.0)),
           ),
@@ -99,41 +99,27 @@ class DrawerToolbar extends StatelessWidget {
   }
 
   Widget _buildEraserButton(BuildContext context, {required bool isSelected}) {
-    return Padding(
-      padding: const EdgeInsets.all(4),
-      child: FloatingActionButton.small(
-        tooltip: "Erase",
-        backgroundColor: const Color(0xFFF7FBFF),
-        elevation: isSelected ? 10 : 2,
-        shape: !isSelected
-            ? const CircleBorder()
-            : RoundedRectangleBorder(borderRadius: BorderRadius.circular(8),),
-        onPressed: notifier.setEraser,
-        child: const Icon(Icons.remove, color: Colors.blueGrey),
-      ),
+    return FloatingActionButton.small(
+      tooltip: "Erase",
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      onPressed: notifier.setEraser,
+      child: isSelected ? Image.network(ImageNetwork.eraserFilledIcon, width: 24, height: 24,)
+          : Image.network(ImageNetwork.eraserIcon, width: 24, height: 24,),
     );
   }
 
   Widget _buildColorButton(
     BuildContext context, {
     required Color color,
-    required ScribbleState state,
-    required Icon icon,
+    required bool isSelected,
   }) {
-    final isSelected = state is DrawerWidget == color.value;
-    return Padding(
-      padding: const EdgeInsets.all(4),
-      child: FloatingActionButton.small(
-          backgroundColor: color,
-          elevation: isSelected ? 10 : 2,
-          shape: !isSelected
-              ? const CircleBorder()
-              : RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
-          child: Container(),
-          onPressed: () => notifier.setColor(color)),
-    );
+    return FloatingActionButton.small(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        child: isSelected ? Image.network(ImageNetwork.penFilledIcon, width: 24, height: 24,)
+            : Image.network(ImageNetwork.penIcon, width: 24, height: 24,),
+        onPressed: () => notifier.setColor(color));
   }
 
   Widget _buildUndoButton(BuildContext context) {
@@ -142,10 +128,7 @@ class DrawerToolbar extends StatelessWidget {
       onPressed: notifier.canUndo ? notifier.undo : null,
       elevation: 0,
       backgroundColor: Colors.transparent,
-      child: const Icon(
-        Icons.undo_rounded,
-        color: Colors.white,
-      ),
+      child: Image.network(ImageNetwork.undoIcon, width: 24, height: 24,),
     );
   }
 
@@ -155,10 +138,7 @@ class DrawerToolbar extends StatelessWidget {
       onPressed: notifier.canRedo ? notifier.redo : null,
       elevation: 0,
       backgroundColor: Colors.transparent,
-      child: const Icon(
-        Icons.redo_rounded,
-        color: Colors.white,
-      ),
+      child: Image.network(ImageNetwork.redoIcon, width: 24, height: 24,),
     );
   }
 
@@ -168,7 +148,7 @@ class DrawerToolbar extends StatelessWidget {
       onPressed: notifier.clear,
       backgroundColor: Colors.transparent,
       elevation: 0,
-      child: const Icon(Icons.delete_forever),
+      child: Image.network(ImageNetwork.binIcon, width: 24, height: 24,),
     );
   }
 }
