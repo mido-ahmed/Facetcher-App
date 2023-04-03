acf18a316fbadd6d4c889bdd07220ac75be9dd67
Henry-Azer <henryazer@outlook.com>
2023-04-04 00:38:12 +0200

    fix get started state path

--- lib/features/app-get-started/presentation/cubit/app_get_started_cubit.dart
+++ lib/features/app-get-started/presentation/cubit/app_get_started_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/usecases/usecase.dart';
 import '../../data/usecases/app_get_started_usecase.dart';
-import 'app_get_started_State.dart';
+import 'app_get_started_state.dart';
 
 class AppGetStartedCubit extends Cubit<AppGetStartedState> {
   final AppGetStartedUseCase appGetStartedUseCase;
