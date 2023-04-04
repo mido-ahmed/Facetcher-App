219739ed9fea2a243943bd8066116227076639c3
Henry-Azer <henryazer@outlook.com>
2023-04-04 03:44:35 +0200

    get rid of user signup repository method

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,12 +1,8 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/exceptions.dart';
-import '../../../../core/models/response_model.dart';
 import '../../../core/usecases/usecase.dart';
-import '../../entities/user/signup.dart';
-import '../../entities/user/user.dart';
 
 abstract class UserRepository {
-  Future<Either<GenericException, ResponseModel<User>>> signup(Signup signup);
   Future<Either<GenericException, NoParams>> setAppGetStarted();
 }
--- lib/data/repositories/user/user_repository_impl.dart
+++ lib/data/repositories/user/user_repository_impl.dart
@@ -2,13 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:facetcher/data/repositories/user/user_repository.dart';
 
 import '../../../core/error/exceptions.dart';
-import '../../../core/models/response_model.dart';
 import '../../../core/network/network_info.dart';
 import '../../../core/usecases/usecase.dart';
 import '../../datasources/user/user_local_datasource.dart';
 import '../../datasources/user/user_remote_datasource.dart';
-import '../../entities/user/signup.dart';
-import '../../entities/user/user.dart';
 
 class UserRepositoryImpl implements UserRepository {
   final NetworkInfo networkInfo;
@@ -19,20 +16,6 @@ class UserRepositoryImpl implements UserRepository {
       {required this.networkInfo,
       required this.userLocalDataSource,
       required this.userRemoteDataSource});
-
-  @override
-  Future<Either<GenericException, ResponseModel<User>>> signup(Signup signup) async {
-    if (await networkInfo.isConnected) {
-      try {
-        final responseCurrentUser = await userRemoteDataSource.signup(signup);
-        return Right(responseCurrentUser);
-      } on GenericException catch (exception) {
-        return Left(exception);
-      }
-    } else {
-      return const Left(CacheException());
-    }
-  }
 
   @override
   Future<Either<GenericException, NoParams>> setAppGetStarted() async {
