e4be377280f763b475e082d1670eb1b890fdec67
Henry-Azer <henryazer@outlook.com>
2023-04-04 03:43:55 +0200

    implement user role model

---
+++ lib/data/models/user/user_role.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class UserRole extends Equatable {
+
+  UserRole({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+
+  late final int id;
+  late final String name;
+  late final String description;
+
+  UserRole.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['description'] = description;
+    return data;
+  }
+
+  @override
+  List<Object?> get props => [id];
+}
