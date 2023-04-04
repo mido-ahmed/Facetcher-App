874a5cfcea39941546872c69fb593e0ef77d327d
Henry-Azer <henryazer@outlook.com>
2023-04-04 03:44:17 +0200

    get rid of user signup remote datasource method

--- lib/data/datasources/user/user_remote_datasource.dart
+++ lib/data/datasources/user/user_remote_datasource.dart
@@ -1,32 +1,9 @@
-import 'dart:async';
-
 import '../../../core/api/api_consumer.dart';
-import '../../../core/api/end_points.dart';
-import '../../../core/error/exceptions.dart';
-import '../../../core/models/response_model.dart';
-import '../../../core/utils/app_strings.dart';
-import '../../entities/user/signup.dart';
-import '../../entities/user/user.dart';
-import '../../models/user/user_model.dart';
 
-abstract class UserRemoteDataSource {
-  Future<ResponseModel<User>> signup(Signup signup);
-}
+abstract class UserRemoteDataSource {}
 
 class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   final ApiConsumer apiConsumer;
 
   UserRemoteDataSourceImpl({required this.apiConsumer});
-
-  @override
-  Future<ResponseModel<User>> signup(Signup signup) async {
-    final response = await apiConsumer.post(EndPoints.signup, body: signup.toJson());
-    if (response[AppStrings.success].toString() == AppStrings.boolFalse) {
-      throw GenericException(message: response[AppStrings.message]);
-    } else {
-      return ResponseModel(
-          success: response[AppStrings.success], message: response[AppStrings.message],
-          body: UserModel.fromJson(response[AppStrings.body]));
-    }
-  }
 }
