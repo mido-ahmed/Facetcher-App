af38ec6c9705e4596bd4009e62fb00289e8d9076
Henry-Azer <henryazer@outlook.com>
2023-04-06 04:47:08 +0200

    adjust face details widget content and integrate with user trial

--- lib/features/drawing-report/presentation/widgets/face_details.dart
+++ lib/features/drawing-report/presentation/widgets/face_details.dart
@@ -1,74 +1,49 @@
+import 'package:facetcher/core/utils/string_util.dart';
 import 'package:flutter/material.dart';
 import 'package:facetcher/core/utils/app_text_style.dart';
 
+import '../../../../data/models/user-trial/user_trial.dart';
+
 class FaceDetails extends StatefulWidget {
-  const FaceDetails({Key? key}) : super(key: key);
+  final UserTrial userTrial;
+  const FaceDetails({Key? key, required this.userTrial}) : super(key: key);
 
   @override
-  _FaceDetailsState createState() => _FaceDetailsState();
+  State<FaceDetails> createState() => _FaceDetailsState();
 }
 
 class _FaceDetailsState extends State<FaceDetails> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.symmetric(vertical: 25.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Details",
-                style: AppTextStyle.textDrawingReport,
-              ),
-              Image.asset('assets/icons/Vector.png'),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
           Row(
             children: [
-              Text(
-                "Title :",
-                style: AppTextStyle.editButtonText,
-              ),
-              Text(
-                "title",
-                style: AppTextStyle.textDescribtion,
-              ),
+              Text("Title: ", style: AppTextStyle.textReportFieldTitle,),
+              Text(widget.userTrial.title, style: AppTextStyle.textReportField,),
             ],
           ),
-          SizedBox(
-            height: 30,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15.0),
+            child: Row(
+              children: [
+                Text("Gender: ", style: AppTextStyle.textReportFieldTitle,),
+                Text(StringUtil.capitalizeFirstLetter(widget.userTrial.gender), style: AppTextStyle.textReportField,),
+              ],
+            ),
           ),
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Gender :",
-                style: AppTextStyle.editButtonText,
-              ),
-              Text(
-                "Male",
-                style: AppTextStyle.textDescribtion,
+              Text("Description: ", style: AppTextStyle.textReportFieldTitle,),
+              Padding(
+                padding: const EdgeInsets.only(left: 2.0),
+                child: Text(widget.userTrial.description, style: AppTextStyle.textReportField,
+                ),
               ),
             ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            "Description :Lorem ipsum dolor  sit amet consectetur. Eleifend fringillaodio iaculis tortor faucibus netus nisl pulvinar mollis. Mattis nisl aliquam  nunc aliquam proin odio. Consequat amet tortor facilisis quam sit. Adipiscing interdum pellentesque elementum ut... ",
-            //maxLines: 5,
-            style: AppTextStyle.textDescribtion,
-          ),
-          SizedBox(
-            height: 10,
           ),
         ],
       ),
