b3774b9f8f43c2cdaae6b1e2d2107f1f1e9cabac
Henry-Azer <henryazer@outlook.com>
2023-04-09 00:40:55 +0200

    add convert date time to date function

--- lib/core/utils/string_util.dart
+++ lib/core/utils/string_util.dart
@@ -1,6 +1,14 @@
+import 'package:intl/intl.dart';
+
 class StringUtil {
   static String capitalizeFirstLetter(String word) {
     if (word.isEmpty) return word;
     return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+  }
+
+  static String convertDateTimeToDate(String dateTimeString) {
+    final dateTime = DateTime.parse(dateTimeString);
+    final formatter = DateFormat('dd-MM-yyyy');
+    return formatter.format(dateTime);
   }
 }
