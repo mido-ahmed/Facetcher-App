9b30e597ea69fd6f590c97fe8256af24e698978b
Henry-Azer <henryazer@outlook.com>
2023-04-09 00:41:19 +0200

    add formatting mobile number function

--- lib/core/utils/string_util.dart
+++ lib/core/utils/string_util.dart
@@ -11,4 +11,14 @@ class StringUtil {
     final formatter = DateFormat('dd-MM-yyyy');
     return formatter.format(dateTime);
   }
+
+  static String formatMobileNumber(String mobileNumber) {
+    final cleaned = mobileNumber.replaceAll(RegExp(r'\D'), '');
+    final groups = <String>[];
+    for (var i = 0; i < cleaned.length; i += 3) {
+      final end = i + 3;
+      groups.add(cleaned.substring(i, end < cleaned.length ? end : cleaned.length));
+    }
+    return groups.join('-');
+  }
 }
