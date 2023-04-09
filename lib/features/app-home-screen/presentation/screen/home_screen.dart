58ca1f155986504f3b999777594545b00d6917fa
Henry-Azer <henryazer@outlook.com>
2023-04-09 19:03:53 +0200

    integrate home screen current user cubit

--- lib/features/app-home-screen/presentation/screen/home_screen.dart
+++ lib/features/app-home-screen/presentation/screen/home_screen.dart
@@ -3,12 +3,19 @@ import 'package:facetcher/core/utils/app_text_style.dart';
 import 'package:facetcher/core/widgets/navigator/navigation_bar_wrapper.dart';
 import 'package:facetcher/features/app-home-screen/presentation/widget/animated_button.dart';
 import 'package:facetcher/features/app-home-screen/presentation/widget/circle_profile.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+import '../../../../config/locale/app_localizations.dart';
 import '../../../../config/routes/app_routes.dart';
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/assets_manager.dart';
+import '../../../../core/utils/constants.dart';
 import '../../../../core/widgets/app_bar_widget.dart';
 import '../../../../core/widgets/icons/animated_icon_button.dart';
+import '../../../../core/widgets/image/network_image_loader.dart';
+import '../../../user-profile/presentation/cubit/current_user_cubit.dart';
+import '../../../user-profile/presentation/cubit/current_user_state.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -27,6 +34,14 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _getCurrentUser();
+  }
+
+  _getCurrentUser() => BlocProvider.of<CurrentUserCubit>(context).findCurrentUser();
+
+  @override
   Widget build(BuildContext context) {
     return NavigationBarWrapper(
       toggleNavigationBar: _toggleNavigationBar,
@@ -38,29 +53,90 @@ class _HomeScreenState extends State<HomeScreen> {
           body: Column(
             children: [
               AppBarWidget(
-                leftChild: Row(
-                  children: [
-                    CircularButton(
-                      function: () {Navigator.pushNamed(context, Routes.userProfile);},
-                      circleRadius: 25.0,
-                      child: Image.network(ImageNetwork.userShape, width: 40,),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 14),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                leftChild: BlocConsumer<CurrentUserCubit, CurrentUserState>(
+                  builder: ((context, state) {
+                    if (state is CurrentUserSuccess && state.user.profilePictureUrl.isNotEmpty) {
+                      return Row(
                         children: [
-                          Text("Hey,User",
-                            style: AppTextStyle.appBarUserProfile,
+                          GestureDetector(
+                            child: CircleAvatar(
+                              backgroundColor: state.user.profilePictureUrl.isEmpty ? AppColors.button : Colors.transparent,
+                              radius: 25.0,
+                              child: state.user.profilePictureUrl.isEmpty
+                                  ? Image.network(ImageNetwork.userShape2, width: 65,)
+                                  : NetworkImageLoader(
+                                      width: 48,
+                                      height: 48,
+                                      url: state.user.profilePictureUrl,
+                                      loader: LoadingAnimationWidget.threeArchedCircle(color: Colors.white, size: 25,),
+                                    ),
+                            ),
+                            onTap: () => { Navigator.pushNamed(context, Routes.userProfile) },
                           ),
-                          Text("Let’s sketch today",
-                            style: AppTextStyle.drawingScreenTitleDetails,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Hey, ${state.user.firstName}", style: AppTextStyle.appBarUserProfile,),
+                                Text("Let’s sketch today", style: AppTextStyle.drawingScreenTitleDetails,),
+                              ],
+                            ),
+                            ),
+                          ],
+                        );
+                    }
+                    if (state is CurrentUserLoading) {
+                      return Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: AppColors.button,
+                            radius: 25.0,
+                            child: Image.network(ImageNetwork.userShape2, width: 22,)
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Hey, ", style: AppTextStyle.appBarUserProfile,),
+                                Text("Let’s sketch today", style: AppTextStyle.drawingScreenTitleDetails,),
+                              ],
+                            ),
                           ),
                         ],
-                      ),
-                    ),
-                  ],
+                      );
+                    }
+                    else {
+                      return Row(
+                        children: [
+                          CircularButton(function: () {Navigator.pushNamed(context, Routes.userProfile);},
+                            circleRadius: 25.0,
+                            child: Image.network(ImageNetwork.userShape, width: 40,),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 14),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Hey, User", style: AppTextStyle.appBarUserProfile,),
+                                Text("Let’s sketch today", style: AppTextStyle.drawingScreenTitleDetails,),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                  }),
+                  listener: ((context, state) {
+                    if (state is CurrentUserError) {
+                      Constants.showSnackBar(context: context, message: AppLocalizations.of(context)!.translate('something_wrong')!);
+                      Navigator.pushReplacementNamed(context, Routes.appSignin);
+                    }
+                  }),
                 ),
                 rightChild: AnimatedIconButton(
                   icon: AnimatedIcons.menu_close,
@@ -77,15 +153,11 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(right: 40.0),
-                      child: Text("From sketch",
-                        style: AppTextStyle.splashTextOne,
-                      ),
+                      child: Text("From sketch", style: AppTextStyle.splashTextOne,),
                     ),
                     Padding(
                       padding: const EdgeInsets.only(left: 100.0),
-                      child: Text("To Life",
-                        style: AppTextStyle.splashTextTwo,
-                      ),
+                      child: Text("To Life", style: AppTextStyle.splashTextTwo,),
                     ),
                   ],
                 ),
@@ -101,20 +173,16 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child:  Row(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Your History",
-                      style: AppTextStyle.homeScreenHistory,
-                    ),
+                    Text("Your History", style: AppTextStyle.homeScreenHistory,),
                     Padding(
                       padding: const EdgeInsets.only(right: 10.0),
                       child: GestureDetector(
-                        // TODO: Naviagte to history
+                        // TODO: Navigate to history
                         onTap: () {},
-                        child: Text("More",
-                          style: AppTextStyle.historyButton,
-                        ),
+                        child: Text("More", style: AppTextStyle.historyButton,),
                       ),
                     ),
                   ],
@@ -135,10 +203,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             height: 150,
                             decoration: BoxDecoration(
                                 color: AppColors.textSecondary,
-                                border: Border.all(
-                                    color: AppColors.historyBorder,
-                                    width: 1.0,
-                                    style: BorderStyle.solid),
+                                border: Border.all(color: AppColors.historyBorder, width: 1.0, style: BorderStyle.solid),
                                 borderRadius: BorderRadius.circular(20)),
                           ),
                           const SizedBox(
