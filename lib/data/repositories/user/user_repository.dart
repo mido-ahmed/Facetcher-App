01fccc4590eb9969f6271a4cd1d97fbb80bb9cb3
Henry-Azer <henryazer@outlook.com>
2023-04-09 17:22:34 +0200

    implement remove user profile picture repository

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,8 +1,10 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/exceptions.dart';
+import '../../../core/models/response_model.dart';
 import '../../../core/usecases/usecase.dart';
 
 abstract class UserRepository {
   Future<Either<GenericException, NoParams>> setAppGetStarted();
+  Future<Either<GenericException, ResponseModel<NoParams>>> removeUserProfilePicture();
 }
--- lib/data/repositories/user/user_repository_impl.dart
+++ lib/data/repositories/user/user_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:facetcher/data/repositories/user/user_repository.dart';
 
 import '../../../core/error/exceptions.dart';
+import '../../../core/models/response_model.dart';
 import '../../../core/network/network_info.dart';
 import '../../../core/usecases/usecase.dart';
 import '../../datasources/user/user_local_datasource.dart';
@@ -24,6 +25,20 @@ class UserRepositoryImpl implements UserRepository {
       return Right(NoParams());
     } on GenericException catch (exception) {
       return Left(CacheException(exception));
+    }
+  }
+
+  @override
+  Future<Either<GenericException, ResponseModel<NoParams>>> removeUserProfilePicture() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final removeResponse = await userRemoteDataSource.removeUserProfilePicture();
+        return Right(removeResponse);
+      } on GenericException catch (exception) {
+        return Left(exception);
+      }
+    } else {
+      return const Left(CacheException());
     }
   }
 }
