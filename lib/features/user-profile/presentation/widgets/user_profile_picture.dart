219830d5034e216cd8ce4f090b174196b7009d66
Henry-Azer <henryazer@outlook.com>
2023-04-09 14:52:11 +0200

    refactor user profile picture components

--- lib/features/user-profile/presentation/widgets/user_profile_picture.dart
+++ lib/features/user-profile/presentation/widgets/user_profile_picture.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 import 'package:facetcher/core/utils/media_query_values.dart';
 import 'package:facetcher/data/models/user/user.dart';
-import 'package:facetcher/features/user-profile/presentation/widgets/profile_widget.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_text_style.dart';
+import '../../../../core/utils/assets_manager.dart';
 
 class UserProfilePicture extends StatefulWidget {
   final User user;
@@ -19,15 +19,14 @@ class UserProfilePicture extends StatefulWidget {
 }
 
 class _UserProfilePictureState extends State<UserProfilePicture> {
-  FilePickerResult? result;
-  PlatformFile? pickedFile;
-  late bool _isExpanded;
+  late FilePickerResult? result;
+  late PlatformFile? pickedFile;
+  late bool _isExpanded = false;
   File? imageFile;
 
   @override
   void initState() {
     super.initState();
-    _isExpanded = false;
     Future.delayed(const Duration(milliseconds: 500), () {
       setState(() {
         _isExpanded = true;
@@ -37,66 +36,142 @@ class _UserProfilePictureState extends State<UserProfilePicture> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      child: Center(
-        child: Stack(
-          children: [
-            SizedBox(
-              width: 160,
-              height: 160,
-              child: ProfileWidget(
-                userProfileName: 'UserName',
-                userEmail: '@username',
-                userProfileUrl: imageFile == null
-                    ? Image.asset('assets/images/1.5x/shape.png')
-                    : ClipRRect(
-                        borderRadius: BorderRadius.circular(150.0),
-                        child: Image.file(
-                          File(imageFile!.path),
-                          height: 300.0,
-                          width: 300.0,
-                          fit: BoxFit.fill,
+    return Center(
+      child: Stack(
+        children: [
+          SizedBox(
+            width: 170,
+            height: 155,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: AppColors.button,
+                  radius: 50.0,
+                  child: widget.user.profilePictureUrl.isEmpty
+                      ? Image.network(ImageNetwork.userShape2, width: 40,)
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(150.0),
+                          child: Image.file(
+                            File(imageFile!.path),
+                            height: 300.0,
+                            width: 300.0,
+                            fit: BoxFit.fill,
+                          ),
                         ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: '${widget.user.firstName} ${widget.user.lastName}',
+                            style: AppTextStyle.userProfileTitle,
+                          ),
+                          TextSpan(
+                            text: '\n${ widget.user.email}',
+                            style: AppTextStyle.userProfileDetails,
+                          ),
+                        ],
                       ),
-              ),
-            ),
-            Positioned(
-              bottom: 20,
-              right: 30,
-              top: 25,
-              child: GestureDetector(
-                child: Container(
-                  height: 30,
-                  width: 30,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      width: 1,
-                      color: AppColors.white,
                     ),
-                    color: AppColors.background.withOpacity(0.4),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 65,
+            right: 30,
+            child: GestureDetector(
+              child: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 0.6, color: AppColors.white,),
+                  color: AppColors.background.withOpacity(0.5),
+                ),
+                child: Icon(Icons.camera_alt_outlined, size: 18, color: AppColors.white,),
+              ),
+              onTap: () async {
+                showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(90.0),
                   ),
-                  child: Icon(
-                    Icons.edit,
-                    size: 20,
-                    color: AppColors.white,
+                  backgroundColor: AppColors.background,
+                  context: context,
+                  builder: (context) => updateProfilePicture(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  updateProfilePicture() {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeInOut,
+      height: _isExpanded ? context.height * 0.20 : context.height * 0.15,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(60),
+          topLeft: Radius.circular(60),
+        ),
+        boxShadow: const [
+          BoxShadow(
+              color: Color.fromRGBO(45, 45, 45, 1),
+              offset: Offset(0, -4),
+              blurRadius: 5,
+              blurStyle: BlurStyle.normal,
+              spreadRadius: 2),
+        ],
+        color: AppColors.background,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 25, bottom: 20, left: 10, right: 10),
+            child: Text('Update Profile Picture', style: AppTextStyle.userProfileTitle,),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(AppColors.button),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0),),
                   ),
                 ),
-                onTap: () async {
-                    showModalBottomSheet(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(90.0),
-                      ),
-                      backgroundColor: AppColors.background,
-                      context: context,
-                      builder: (context) => uploadingProfilePicture(takenPhoto),
-                    );
-                  }
+                onPressed: () async {
+                  takenPhoto();
+                },
+                icon: Icon(Icons.image, color: AppColors.white),
+                label: Text('Upload', style: AppTextStyle.userProfileInfo,),
               ),
-            ),
-          ],
-        ),
+              const SizedBox(width: 20.0,),
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(AppColors.removeButton),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0),),
+                  ),
+                ),
+                onPressed: () {},
+                label: Text('Remove', style: AppTextStyle.userProfileInfo,),
+                icon: Icon(Icons.delete_forever, color: AppColors.white),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
@@ -112,71 +187,10 @@ class _UserProfilePictureState extends State<UserProfilePicture> {
     File? img = File(pickedFile!.path.toString());
     img = await _cropImage(imageFile: img);
     setState(
-      () {
+          () {
         imageFile = img;
         Navigator.of(context).pop();
       },
-    );
-
-  }
-
-  uploadingProfilePicture(Function() onTap) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 600),
-      curve: Curves.easeInOut,
-      height: _isExpanded
-          ? context.height * 0.20
-          : context.height * 0.15,
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.only(
-            topRight: Radius.circular(70),
-            topLeft: Radius.circular(70),
-          ),
-          boxShadow: const [
-            BoxShadow(
-                color: Color.fromRGBO(48, 48, 48, 1),
-                offset: Offset(0, -5),
-                blurRadius: 8,
-                blurStyle: BlurStyle.normal,
-                spreadRadius: 2),
-          ],
-          color: AppColors.background),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding:
-                const EdgeInsets.only(top: 40, bottom: 20, left: 10, right: 10),
-            child: Text(
-              'Choose Your Profile Picture',
-              style: AppTextStyle.userProfileTitle,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(AppColors.button),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                ),
-                onPressed: () async {
-                  takenPhoto();
-                },
-                icon: Icon(
-                  Icons.image,
-                  color: AppColors.white,
-                ),
-                label: Text('Gallery', style: AppTextStyle.userProfileInfo,),
-              )
-            ],
-          )
-        ],
-      ),
     );
   }
 
@@ -193,17 +207,14 @@ class _UserProfilePictureState extends State<UserProfilePicture> {
       uiSettings: [
         AndroidUiSettings(
             activeControlsWidgetColor: AppColors.button,
-            toolbarTitle: 'Cropper',
+            toolbarTitle: 'Image Cropper',
             toolbarColor: AppColors.background,
-            toolbarWidgetColor: AppColors.button,
+            toolbarWidgetColor: AppColors.textPrimary,
             initAspectRatio: CropAspectRatioPreset.original,
-            lockAspectRatio: false),
-        IOSUiSettings(
-          title: 'Cropper',
+            lockAspectRatio: false
         ),
-        WebUiSettings(
-          context: context,
-        ),
+        IOSUiSettings(title: 'Image Cropper',),
+        WebUiSettings(context: context,),
       ],
     );
     if (croppedImage == null) return null;
