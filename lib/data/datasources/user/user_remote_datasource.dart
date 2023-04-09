bcea5492606a9ef3e6fa70c028214e8b5c2ce35b
Henry-Azer <henryazer@outlook.com>
2023-04-09 18:01:25 +0200

    implement upload user profile picture remote datasource

--- lib/data/datasources/user/user_remote_datasource.dart
+++ lib/data/datasources/user/user_remote_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import '../../../core/api/api_consumer.dart';
 import '../../../core/api/end_points.dart';
 import '../../../core/error/exceptions.dart';
@@ -6,6 +8,7 @@ import '../../../core/usecases/usecase.dart';
 import '../../../core/utils/app_strings.dart';
 
 abstract class UserRemoteDataSource {
+  Future<ResponseModel<NoParams>> uploadUserProfilePicture(Uint8List image);
   Future<ResponseModel<NoParams>> removeUserProfilePicture();
 }
 
@@ -13,6 +16,17 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   final ApiConsumer apiConsumer;
 
   UserRemoteDataSourceImpl({required this.apiConsumer});
+
+  @override
+  Future<ResponseModel<NoParams>> uploadUserProfilePicture(Uint8List image) async {
+    final response = await apiConsumer.postFormData(EndPoints.userProfilePicture, body: {'photo' : image});
+    if (response[AppStrings.success].toString() == AppStrings.boolFalse) {
+      throw GenericException(message: response[AppStrings.message]);
+    } else {
+      return ResponseModel(success: response[AppStrings.success],
+          message: response[AppStrings.message], body: NoParams());
+    }
+  }
 
   @override
   Future<ResponseModel<NoParams>> removeUserProfilePicture() async {
