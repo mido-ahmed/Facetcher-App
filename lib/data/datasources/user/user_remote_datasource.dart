f18a43b266e664c44af93b1d2d59c1c3340889e7
Henry-Azer <henryazer@outlook.com>
2023-04-09 20:52:55 +0200

    implement change user password remote data source

--- lib/data/datasources/user/user_remote_datasource.dart
+++ lib/data/datasources/user/user_remote_datasource.dart
@@ -1,5 +1,7 @@
 import 'dart:typed_data';
 
+import 'package:facetcher/data/entities/user/user_change_password_request.dart';
+
 import '../../../core/api/api_consumer.dart';
 import '../../../core/api/end_points.dart';
 import '../../../core/error/exceptions.dart';
@@ -8,6 +10,7 @@ import '../../../core/usecases/usecase.dart';
 import '../../../core/utils/app_strings.dart';
 
 abstract class UserRemoteDataSource {
+  Future<ResponseModel<NoParams>> changeUserPassword(UserChangePasswordRequest userChangePasswordRequest);
   Future<ResponseModel<NoParams>> uploadUserProfilePicture(Uint8List image);
   Future<ResponseModel<NoParams>> removeUserProfilePicture();
 }
@@ -16,6 +19,18 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   final ApiConsumer apiConsumer;
 
   UserRemoteDataSourceImpl({required this.apiConsumer});
+
+
+  @override
+  Future<ResponseModel<NoParams>> changeUserPassword(UserChangePasswordRequest userChangePasswordRequest) async {
+    final response = await apiConsumer.put(EndPoints.userPassword, body: userChangePasswordRequest.toJson());
+    if (response[AppStrings.success].toString() == AppStrings.boolFalse) {
+      throw GenericException(message: response[AppStrings.message]);
+    } else {
+      return ResponseModel(success: response[AppStrings.success],
+          message: response[AppStrings.message], body: NoParams());
+    }
+  }
 
   @override
   Future<ResponseModel<NoParams>> uploadUserProfilePicture(Uint8List image) async {
