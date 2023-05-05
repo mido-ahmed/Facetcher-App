8346bfd2f806754d012e3819d7a1ecd22f1c8b5b
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-05-06 01:06:31 +0300

    first screen in indicator screens finished

---
+++ lib/features/app-indicator-screen/presentation/screens/indicator_start_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/indicator_screen.dart';
+
+class indicatorStartScreen extends StatefulWidget {
+  const indicatorStartScreen({Key? key}) : super(key: key);
+
+  @override
+  State<indicatorStartScreen> createState() => _indicatorStartScreenState();
+}
+
+class _indicatorStartScreenState extends State<indicatorStartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        extendBody: true,
+        resizeToAvoidBottomInset: true,
+        body: IndicatorScreen(
+          isTop: false,
+          imageUrl: "https://d178ormcbshsy5.cloudfront.net/intro_01.png",
+          description:
+              "Turn your sketches into realistic portraits with just a few clicks !",
+          title: "Start Facetchering",
+        ),
+      ),
+    );
+  }
+}
