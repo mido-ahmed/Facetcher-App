deff7ed759f647598292c827ede3a6f62846fa5a
mido-ahmed <220190077@fa-hists.edu.eg>
2023-05-18 02:14:56 +0300

    create message us modeling file in entities folder

---
+++ lib/data/entities/message-us/message_us.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class MessageUsRequest extends Equatable {
+  final String title;
+  final String message;
+
+  const MessageUsRequest({required this.title, required this.message});
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+  Map<String, dynamic> toJson() => {"title": title, "message": message};
+}
