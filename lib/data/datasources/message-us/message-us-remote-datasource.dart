4ee380aecf44fd56b630cc9dee6e0ce8b65217e2
mido-ahmed <220190077@fa-hists.edu.eg>
2023-05-18 02:17:35 +0300

    create message us local and remote data source

---
+++ lib/data/datasources/message-us/message-us-remote-datasource.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:facetcher/data/entities/message-us/message_us.dart';
+import 'package:facetcher/data/models/message-us/message-us.dart';
+
+import '../../../core/api/api_consumer.dart';
+import '../../../core/api/end_points.dart';
+import '../../../core/error/exceptions.dart';
+import '../../../core/models/response_model.dart';
+import '../../../core/utils/app_strings.dart';
+
+abstract class MessageUsRemoteDataSource {
+  Future<ResponseModel<MessageUs>> createUserMessage(
+      MessageUsRequest messageUsRequest);
+}
+
+class MessageUsRemoteDataSourceImpl implements MessageUsRemoteDataSource {
+  final ApiConsumer apiConsumer;
+
+  MessageUsRemoteDataSourceImpl({required this.apiConsumer});
+
+  @override
+  Future<ResponseModel<MessageUs>> createUserMessage(
+      MessageUsRequest messageUsRequest) async {
+    final response = await apiConsumer.postFormData(EndPoints.createUserTrial,
+        body: messageUsRequest.toJson());
+    if (response[AppStrings.success].toString() == AppStrings.boolFalse) {
+      throw GenericException(message: response[AppStrings.message]);
+    } else {
+      return ResponseModel(
+          success: response[AppStrings.success],
+          message: response[AppStrings.message],
+          body: MessageUs.fromJson(response[AppStrings.body]));
+    }
+  }
+}
