9275a339523762748948cfc53b10be07659dae98
mido-ahmed <220190077@fa-hists.edu.eg>
2023-05-18 01:08:17 +0300

    add the new screen

---
+++ lib/features/app-report-problem/presentation/screens/report_problem.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/widgets/app_bar_widget.dart';
+import '../../../../core/widgets/icons/animated_icon_button.dart';
+import '../../../../core/widgets/navigator/navigation_bar_wrapper.dart';
+import '../widgets/report_problem_form.dart';
+
+class ReportProblemScreen extends StatefulWidget {
+  const ReportProblemScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ReportProblemScreen> createState() => _ReportProblemScreenState();
+}
+
+class _ReportProblemScreenState extends State<ReportProblemScreen> {
+  bool _toggleNavigationBar = false;
+
+  void _handleToggleNavigationBar() {
+    setState(() {
+      _toggleNavigationBar = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBarWrapper(
+      toggleNavigationBar: _toggleNavigationBar,
+      path: ModalRoute.of(context)?.settings.name,
+      child: Material(
+        child: Scaffold(
+          extendBody: true,
+          resizeToAvoidBottomInset: true,
+          body: SingleChildScrollView(
+            physics: const ScrollPhysics(),
+            child: Column(
+              children: [
+                AppBarWidget(
+                  leftChild: IconButton(
+                    icon: Icon(Icons.arrow_back, color: AppColors.white),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  rightChild: AnimatedIconButton(
+                    icon: AnimatedIcons.menu_close,
+                    color: AppColors.fontPrimary,
+                    onPressed: () => _handleToggleNavigationBar(),
+                    durationMilliseconds: 500,
+                    size: 32.0,
+                    end: 1.0,
+                  ),
+                ),
+                const ReportProblemForm()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
