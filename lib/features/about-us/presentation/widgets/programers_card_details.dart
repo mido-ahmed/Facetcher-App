a7364c2ecef30ae0a5e2b4a7b0afbc9bc1c14f56
Tota <71022738+Tota6@users.noreply.github.com>
2023-05-19 17:26:08 +0300

    programmer input details

---
+++ lib/features/about-us/presentation/widgets/programers_card_details.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProgrammerInfo {
+  final String programmerName;
+  final String programmerImage;
+  const ProgrammerInfo({Key? key, required this.programmerName,required this.programmerImage}) ;
+
+}
